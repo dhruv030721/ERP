7c56191e69159807516cefcc44c978e847728e18
Dhruv Godhani <dhruvgodhani07@gmail.com>
2024-12-11 21:05:44 +0530

    - Add SSL in backend

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -5,12 +5,18 @@ import routes from './Utils/router';
 import logger from "./Utils/logger";
 import morgan from "morgan";
 import cors from "cors";
+import fs from 'fs';
+import https from 'https';
 
 dotenv.config();
 const prisma = new PrismaClient();
 const app: Application = express();
 const PORT = process.env.PORT || 8888;
 
+const sslOptions = {
+    key: fs.readFileSync('./server.key'),
+    cert: fs.readFileSync('./server.cert')
+}
 app.use(
     cors({
         origin: ["http://localhost:5173", "http://37.27.81.8:4001"],
@@ -26,7 +32,7 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Welcome to Express & TypeScript Server');
 });
 
-app.listen(PORT, () => {
+https.createServer(sslOptions, app).listen(PORT, () => {
     logger.info(`Server started successfully at ${PORT}`);
     prisma.$connect().then(() => {
         logger.info("Database connected successfully");
